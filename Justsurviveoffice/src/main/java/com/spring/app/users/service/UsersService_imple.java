252f3764508ad17c52d5c1637240ac5e46725eae
dongdangs <shindonghee2@naver.com>
2025-08-19 14:12:05 +0900

    아이디찾기 이메일찾기 암호화 완료.

--- Justsurviveoffice/src/main/java/com/spring/app/users/service/UsersService_imple.java
+++ Justsurviveoffice/src/main/java/com/spring/app/users/service/UsersService_imple.java
@@ -37,43 +37,42 @@ public class UsersService_imple implements UsersService {
        
 
    @Override
-      public UsersDTO getUser(String id, String Pwd) {
+   public UsersDTO getUser(String id, String Pwd) {
          
-         UsersDTO usersDto = null;
+     UsersDTO usersDto = null;
          
-         try {
-            Optional<Users> user = usersRepository.findById(id);
-   /*         Java8에서는 Optional<T> 클래스를 사용해 NullPointerException 을 방지할 수 있도록 도와준다. 
-              Optional<T>는 null이 올 수 있는 값을 감싸는 Wrapper 클래스 이므로, 참조하더라도 NullPointerException 이 발생하지 않도록 도와준다. 
-              Optional 클래스는 null 이더라도 바로 NullPointerException 이 발생하지 않으며, 클래스이기 때문에 각종 메소드를 제공해준다. */
-			
-	        aes = new AES256(SecretMyKey.KEY);
-	        
-            Users users = user.get();
-            // java.util.Optional.get() 은 값이 존재하면 값을 리턴시켜주고, 값이 없으면 NoSuchElementException 을 유발시켜준다.
-            
-            
-               try {
-                  usersDto = users.toDTO();
-                  System.out.println(usersDto.getId());
-                  System.out.println(Sha256.encrypt(Pwd));
-                  // usersDto.setPassword(Sha256.encrypt(Pwd));
-                  
-     			 // 복호화해서 DTO에 담기
-     	        usersDto.setEmail(aes.decrypt(users.getEmail()));
-     	        usersDto.setMobile(aes.decrypt(users.getMobile()));
-     	        
-               } catch (Exception e) {   
-                  e.printStackTrace();
-               }
-            
-         } catch(NoSuchElementException e) {
-            // member.get() 에서 데이터가 존재하지 않는 경우
-         } catch (Exception e) {
-			e.printStackTrace();
-		}
-         return usersDto;
-      } 
+     try {
+        Optional<Users> user = usersRepository.findById(id);
+   /*   Java8에서는 Optional<T> 클래스를 사용해 NullPointerException 을 방지할 수 있도록 도와준다. 
+        Optional<T>는 null이 올 수 있는 값을 감싸는 Wrapper 클래스 이므로, 참조하더라도 NullPointerException 이 발생하지 않도록 도와준다. 
+        Optional 클래스는 null 이더라도 바로 NullPointerException 이 발생하지 않으며, 클래스이기 때문에 각종 메소드를 제공해준다. */
+        aes = new AES256(SecretMyKey.KEY);
+        
+        Users users = user.get();
+        // java.util.Optional.get() 은 값이 존재하면 값을 리턴시켜주고, 값이 없으면 NoSuchElementException 을 유발시켜준다.
+        
+        
+           try {
+              usersDto = users.toDTO();
+              System.out.println(usersDto.getId());
+              System.out.println(Sha256.encrypt(Pwd));
+              // usersDto.setPassword(Sha256.encrypt(Pwd));
+              
+ 			 // 복호화해서 DTO에 담기
+ 	        usersDto.setEmail(aes.decrypt(users.getEmail()));
+ 	        usersDto.setMobile(aes.decrypt(users.getMobile()));
+ 	        
+           } catch (Exception e) {   
+              e.printStackTrace();
+           }
+        
+     } catch(NoSuchElementException e) {
+        // member.get() 에서 데이터가 존재하지 않는 경우
+     } catch (Exception e) {
+		e.printStackTrace();
+	}
+     return usersDto;
+  } 
 
    
    // 아이디 중복 체크
@@ -97,7 +96,6 @@ public class UsersService_imple implements UsersService {
 	}
    
 
-
    // 회원가입
    @Override
    public void registerUser(Users user) {
@@ -115,24 +113,18 @@ public class UsersService_imple implements UsersService {
    }
 
 
-   // 유저 존재 여부 확인
-   @Override
-   public Users findByIdAndEmail(String id, String email) {
-      return usersRepository.findByIdAndEmail(id, email);
-   }
-
-
    // 비밀번호 업데이트
    @Override
    public void updatePassword(String id, String newPassword) {
+	   
+       Users user = usersRepository.findById(id).orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
+       
+       user.setPassword(Sha256.encrypt(newPassword));
       
-      Users user = usersRepository.findById(id).orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
-      user.setPassword(Sha256.encrypt(newPassword));
-      
-      user.setPasswordChanged(LocalDateTime.now());   // 변경일 갱신
+       user.setPasswordChanged(LocalDateTime.now());   // 변경일 갱신
        user.setIsDormant(0);                           // 휴면 해제
 
-      usersRepository.save(user);
+       usersRepository.save(user);
    }
 
    //회원 수정하기
@@ -220,33 +212,55 @@ public class UsersService_imple implements UsersService {
 
     @Override
     public UsersDTO getIdFind(String name, String email) {
-           return usersRepository.findByNameAndEmail(name.trim(), email.trim())
-                   .map(Users::toDTO) // Users 엔티티에 toDTO() 있다고 가정
-                   .orElse(null);
+    	
+    	String encryptEmail = "";
+    	
+    	try {
+    		aes = new AES256(SecretMyKey.KEY);
+    		encryptEmail = aes.encrypt(email.trim());
+		} catch (UnsupportedEncodingException | GeneralSecurityException e) {
+			e.printStackTrace();
+		}
+    	
+        return usersRepository.findByNameAndEmail(name.trim(), encryptEmail)
+        					  .map(Users::toDTO) // Users 엔티티에 toDTO() 있다고 가정
+    					  	  .orElse(null);
+    }
+    
+    // 유저 존재 여부 확인
+    @Override
+    public Users findByIdAndEmail(String id, String email) {
+    	String encryptEmail = "";
+    	try {
+    		aes = new AES256(SecretMyKey.KEY);
+    		encryptEmail = aes.encrypt(email.trim());
+		} catch (UnsupportedEncodingException | GeneralSecurityException e) {
+			e.printStackTrace();
+		}
+    	return usersRepository.findByIdAndEmail(id, encryptEmail);
     }
 
-
-   //휴면처리하기 ( 비밀번호변경대상 )
-   @Override
-   public boolean updateDormantStatus(String id) {
+    //휴면처리하기 ( 비밀번호변경대상 )
+    @Override
+    public boolean updateDormantStatus(String id) {
          
        Users users = usersRepository.findById(id).orElse(null);
        //orElse는 객체꺼내기를 시도할때 값이 없으면 null 을 반환하라는 뜻 !
       
-      LocalDateTime now = LocalDateTime.now();
+       LocalDateTime now = LocalDateTime.now();
       
-      if(users.getPasswordChanged() == null || 
-         users.getPasswordChanged().isBefore(now.minusYears(1))) {
+       if(users.getPasswordChanged() == null || 
+          users.getPasswordChanged().isBefore(now.minusYears(1))) {
             
-         users.setIsDormant(1);
-         usersRepository.save(users);
+          users.setIsDormant(1);
+          usersRepository.save(users);
          
-         return true;
+          return true;
          
-      }
+       }
       
-      return false;
-   }
+       return false;
+    }
 
 
    
